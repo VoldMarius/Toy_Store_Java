41004173ebede9cbd7777032974e46a8dc734819
Vol.d'Marius <115695164+VoldMarius@users.noreply.github.com>
2023-06-11 09:19:25 +0300

    Добавил описания к дейсвиям кода

--- src/Toy.java
+++ src/Toy.java
@@ -11,16 +11,31 @@ public class Toy {
     private PriorityQueue<String> queue;
 
     public Toy(String ids, String names, String weights) {
-        this.toyIds = ids.split(" ");
-        this.toyNames = names.split(" ");
-        String[] weightString = weights.split(" ");
-        this.toyWeights = new int[weightString.length];
+        this.toyIds = ids.split(" ");//ids — это строка, в которой идентификаторы игрушек разделены пробелами,
+        // каждый элемент массива toyIds соответствует идентификатору одной игрушки в том же порядке, что и их имена
+        // и вес в массивах toyNames и toyWeights.
+
+        this.toyNames = names.split(" ");//names — это строка, в которой имена игрушек разделены пробелами,
+        //  каждый элемент массива toyNames соответствует имени одной игрушки в том же порядке,
+        // что и их идентификаторы и вес в массивах toyIds и toyWeights.
+
+        String[] weightString = weights.split(" "); //weights — это строка, в которой веса игрушек
+        // разделены пробелами, каждый элемент массива toyWeights соответствует весу одной игрушки в том же порядке,
+        // что и их идентификаторы и имена в массивах toyIds и toyNames.
+
+        this.toyWeights = new int[weightString.length]; //Эта строка кода создает массив целых чисел toyWeights
+        // длиной weightString.length. Переменная weightString представляет собой массив строк,
+        // содержащий вес игрушек в порядке их идентификаторов и имен. В этой строке кода создается новый массив
+        // целых чисел для хранения веса игрушек.
         this.queue = new PriorityQueue<>();
 
-        for (int i = 0; i < weightString.length; i++) {
+        for (int i = 0; i < weightString.length; i++) { // данный цикл проходит по всем элементам массива weightString,
+            // преобразует их в целые числа сохраняет их в массив toyWeights объекта, а также добавляет элементы
+            // массива toyIds в очередь queue объекта.
             this.toyWeights[i] = Integer.parseInt(weightString[i]);
             this.queue.add(this.toyIds[i]);
         }
+
     }
 
     public String get() {
@@ -30,7 +45,11 @@ public class Toy {
             return this.toyIds[0];
         } else if (rnd <= 40) {
             return this.toyIds[1];
-        } else {
+        } else { // (если rnd больше 40), генерируется случайный индекс элемента в очереди queue объекта,
+            // сохраняется в переменной randomIndex, преобразуется в массив методом toArray(),
+            // и затем возвращается этот случайный элемент. queue.toArray(new String[queue.size()])[randomIndex]
+            // преобразует очередь queue в массив объектов типа String, выбирает случайный элемент в массиве по индексу
+            // randomIndex и возвращает его в качестве результата выполнения метода get().
             int randomIndex = rand.nextInt(queue.size());
             return queue.toArray(new String[queue.size()])[randomIndex];
         }
